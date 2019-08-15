a849eb4f47e8dd6879eda79c05709ccd639c0990
github username <github email address>
2019-08-15 14:40:20 -0500

    cat-car update

--- cat-carousel.js
+++ cat-carousel.js
@@ -1,9 +1,16 @@
 'use strict';
 
+function thumbnailValue(){
 $('.thumbnail').on('click', e => {
-  console.log(e.target);
+  //console.log(e.target);
+  
+  let imgSrc = $(this).find('img').attr('src');
+  let imgAlt = $(this).find('img').attr('alt');
+  
+  $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+
 });
 
-$('.thumbnail').on('click', event => {
-    event
-});
+}
+
+$(thumbnailValue);
