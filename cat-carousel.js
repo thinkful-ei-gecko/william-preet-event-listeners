59cfd94bf59149275b0517e2028c6c9e6bbab45f
William Bae <wbae912@gmail.com>
2019-08-15 13:09:58 -0700

    cat carousel complete

--- cat-carousel.js
+++ cat-carousel.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function thumbnailValue(){
-$('.thumbnail').on('click', e => {
+  $('.thumbnail').on('click', e => {
   //console.log(e.target);
   
-  let imgSrc = $(this).find('img').attr('src');
-  let imgAlt = $(this).find('img').attr('alt');
-  
-  $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+    let imgSrc = $(e.currentTarget).find('img').attr('src');
+    let imgAlt = $(e.currentTarget).find('img').attr('alt');
 
-});
+    $('.hero img').attr({'src': imgSrc, 'alt': imgAlt});
+
+  });
 
 }
 
